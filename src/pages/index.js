97aa768d1cff4a36fc634078c657b8518ba9f943
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-01-23 14:32:28 +0700

    moved to SSR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
-import Index from "@/pages/products";
+import {Index} from "@/pages/products";
+import axios from "axios";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const response = await axios.get(`https://dummyjson.com/products?limit=10`);
+  const {data} = response;
+
+  return {
+    props: {
+      products: data.products,
+    }
+  }
+}
+
+export default function Home({products}) {
   return (
     <>
-      <Index/>
+      <Index products={products}/>
     </>
   )
 }
