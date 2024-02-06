7131988377612b35148e24e94fdb0f0072a657fa
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-02-06 22:20:47 +0700

    formatted code

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,7 +19,7 @@ const Menu = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
+          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Shop Logo"/>
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">JSHOP</span>
         </Link>
         <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
@@ -36,7 +36,7 @@ const Menu = () => {
               </>
               :
               <>
-                <Link href="/login"
+                <Link href={'/login'}
                       className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
                   Login
                 </Link>
