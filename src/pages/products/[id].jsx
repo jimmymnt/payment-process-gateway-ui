57ca5ba9b1099a7947fae4795cbafd1a6d6456bf
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-02-06 23:50:55 +0700

    added animation loading for Product Details page

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -8,6 +8,8 @@ import api from "@/utils/Api";
 import ProductCarouselThumbs from "@/components/Product/ProductCarouselThumbs";
 import ProductImage from "@/components/Product/ProductImage";
 import ProductCategory from "@/components/Product/ProductCategory";
+import Loading from "@/components/Loading";
+import LoadingAnimation from "@/components/Utils/LoadingAnimation";
 
 const ProductDetails = () => {
   const router = useRouter();
@@ -29,20 +31,20 @@ const ProductDetails = () => {
     const secDelay = loadStripeComponent ? 0 : 1000;
     setTimeout(async () => {
       event.preventDefault();
-      const paymentIntentInfo = localStorage.getItem(`pi_${product.id}`);
-      if (!paymentIntentInfo) {
-        const paymentIntent = await createPaymentIntent(product);
-        localStorage.setItem(`pi_${product.id}`, JSON.stringify(paymentIntent.data));
-        setClientSecret(paymentIntent.data.client_secret);
-      } else {
-        const data = JSON.parse(paymentIntentInfo);
-        setClientSecret(data.client_secret);
-      }
+      // const paymentIntentInfo = localStorage.getItem(`pi_${product.id}`);
+      // if (!paymentIntentInfo) {
+      //   const paymentIntent = await createPaymentIntent(product);
+      //   localStorage.setItem(`pi_${product.id}`, JSON.stringify(paymentIntent.data));
+      //   setClientSecret(paymentIntent.data.client_secret);
+      // } else {
+      //   const data = JSON.parse(paymentIntentInfo);
+      //   setClientSecret(data.client_secret);
+      // }
 
       /// The payment form needs to be waited until the Payment Intent is created.
-      setLoadStripeComponent((oldValue) => !oldValue);
+      // setLoadStripeComponent((oldValue) => !oldValue);
       setLoading(false);
-    }, secDelay);
+    }, 3000);
   }
 
   useEffect(() => {
@@ -149,23 +151,34 @@ const ProductDetails = () => {
                         disabled={loading}>
                   {
                     loading &&
-                    <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-                         fill="none"
-                         viewBox="0 0 24 24">
-                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                              strokeWidth="4"></circle>
-                      <path className="opacity-75" fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                      </path>
-                    </svg>
+                    <LoadingAnimation/>
                   }
                   {loading ? 'Loading' : 'Pay with Stripe'}
                 </button>
               </div>
-              <button
-                type="button"
-                className="w-full mt-6 px-4 py-3 bg-[#333] hover:bg-[#222] text-white rounded-full">
-                Add to cart
+              <button type="button"
+                      className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                     stroke="currentColor" className="file: mr-2 h-6 w-6">
+                  <path strokeLinecap="round" strokeLinejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
+                </svg>
+                <span>Add to cart</span>
+              </button>
+              <button type="button"
+                      disabled={loading}
+                      onClick={loadPaymentForm}
+                      className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">
+                {
+                  loading ?
+                    <LoadingAnimation/>
+                    :
+                    <svg xmlns="http://www.w3.org/2000/svg" className="file: mr-2 h-6 w-6" viewBox="0 0 8 8">
+                      <path fill="currentColor"
+                            d="M2.25 1C.11 1 0 1.11 0 1.25V2h8v-.75C8 1.11 7.89 1 7.75 1zM0 3v3.75c0 .14.11.25.25.25h7.5c.14 0 .25-.11.25-.25V3zm1 2h1v1H1zm2 0h1v1H3z"/>
+                    </svg>
+                }
+                <span>Pay with Stripe</span>
               </button>
             </div>
           </div>
@@ -174,7 +187,7 @@ const ProductDetails = () => {
         {
           loadStripeComponent &&
           <Elements stripe={stripePromise} options={options}>
-            <h2
+          <h2
               className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-600 md:text-5xl lg:text-4xl text-center">
               Lets make a payment
             </h2>
