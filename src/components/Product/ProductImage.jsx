a268535d03c2f46dc09d703ef9550bba23597bb0
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-02-07 18:36:13 +0700

    removed redundant code

--- src/components/Product/ProductImage.jsx
+++ src/components/Product/ProductImage.jsx
@@ -1,8 +1,5 @@
 "use client"
 import {useState, useRef, useEffect} from 'react'
-import Image from 'next/image'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 import {Carousel} from "flowbite-react";
 
 const ProductImage = ({images}) => {
@@ -31,55 +28,6 @@ const ProductImage = ({images}) => {
           }
         </Carousel>
       </div>
-      {/*<div className="relative h-96">*/}
-      {/*  {*/}
-      {/*    mainImg && <Image*/}
-      {/*      src={mainImg}*/}
-      {/*      alt={mainImg}*/}
-      {/*      fill*/}
-      {/*      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"*/}
-      {/*      className="object-contain transform cursor-pointer"*/}
-      {/*    />}*/}
-      {/*</div>*/}
-      {/*<div className="relative flex border-t border-palette-lighter">*/}
-      {/*  <button*/}
-      {/*    aria-label="left-scroll"*/}
-      {/*    className="h-32 bg-gray-200 hover:bg-palette-light absolute left-0 z-10 opacity-75"*/}
-      {/*    onClick={() => scroll(-200)}*/}
-      {/*  >*/}
-      {/*    <FontAwesomeIcon icon={faArrowLeft} className="w-3 mx-1 text-palette-primary"/>*/}
-      {/*  </button>*/}
-      {/*  <div*/}
-      {/*    ref={ref}*/}
-      {/*    style={{scrollBehavior: "smooth"}}*/}
-      {/*    className="flex space-x-1 gap-5 w-full overflow-auto scrollbar-hide border-t border-palette-lighter"*/}
-      {/*  >*/}
-      {/*    {*/}
-      {/*      images && images.map((imgItem, index) => (*/}
-      {/*        <button*/}
-      {/*          key={index}*/}
-      {/*          className="relative w-40 h-32 flex-shrink-0 rounded-sm border border-palette-lighter"*/}
-      {/*          onClick={() => setMainImg(imgItem)}*/}
-      {/*        >*/}
-      {/*          <Image*/}
-      {/*            src={imgItem}*/}
-      {/*            alt={imgItem}*/}
-      {/*            fill*/}
-      {/*            className="object-cover"*/}
-      {/*            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"*/}
-      {/*          />*/}
-      {/*        </button>*/}
-      {/*      ))*/}
-      {/*    }*/}
-      {/*  </div>*/}
-      {/*  <button*/}
-      {/*    aria-label="right-scroll"*/}
-      {/*    className="h-32 bg-gray-200 hover:bg-palette-light absolute right-0 z-10 opacity-75"*/}
-      {/*    onClick={() => scroll(200)}*/}
-      {/*  >*/}
-      {/*    <FontAwesomeIcon icon={faArrowRight} className="w-3 mx-1 text-palette-primary"/>*/}
-      {/*  </button>*/}
-      {/*</div>*/}
     </div>
   )
 }
