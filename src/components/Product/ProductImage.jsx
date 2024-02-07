d4abb7897e9d9ef35b6095e47fd64bfbc4496e3f
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-02-07 18:35:23 +0700

    added product card and custom product details page'

--- src/components/Product/ProductImage.jsx
+++ src/components/Product/ProductImage.jsx
@@ -1,7 +1,9 @@
+"use client"
 import {useState, useRef, useEffect} from 'react'
 import Image from 'next/image'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import {Carousel} from "flowbite-react";
 
 const ProductImage = ({images}) => {
   const [mainImg, setMainImg] = useState(null)
@@ -16,56 +18,68 @@ const ProductImage = ({images}) => {
   }, [images]);
 
   return (
-    <div className="w-full md:w-1/2 border border-palette-lighter bg-white rounded shadow-lg dark:border-gray-600 dark:bg-gray-600">
-      <div className="relative h-96">
-        {
-          mainImg && <Image
-            src={mainImg}
-            alt={mainImg}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-contain transform cursor-pointer"
-          />}
-      </div>
-      <div className="relative flex border-t border-palette-lighter">
-        <button
-          aria-label="left-scroll"
-          className="h-32 bg-gray-200 hover:bg-palette-light absolute left-0 z-10 opacity-75"
-          onClick={() => scroll(-200)}
-        >
-          <FontAwesomeIcon icon={faArrowLeft} className="w-3 mx-1 text-palette-primary"/>
-        </button>
-        <div
-          ref={ref}
-          style={{scrollBehavior: "smooth"}}
-          className="flex space-x-1 gap-5 w-full overflow-auto scrollbar-hide border-t border-palette-lighter"
-        >
+    <div
+      className="w-full md:w-1/2 bg-white rounded dark:border-gray-600 dark:bg-gray-800">
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slideInterval={4000}>
           {
-            images && images.map((imgItem, index) => (
-              <button
-                key={index}
-                className="relative w-40 h-32 flex-shrink-0 rounded-sm border border-palette-lighter"
-                onClick={() => setMainImg(imgItem)}
-              >
-                <Image
-                  src={imgItem}
-                  alt={imgItem}
-                  fill
-                  className="object-cover"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                />
-              </button>
-            ))
+            images && images.map((imgItem, index) => {
+              return (
+                  <img key={index} src={imgItem} alt="..."/>
+              )
+            })
           }
-        </div>
-        <button
-          aria-label="right-scroll"
-          className="h-32 bg-gray-200 hover:bg-palette-light absolute right-0 z-10 opacity-75"
-          onClick={() => scroll(200)}
-        >
-          <FontAwesomeIcon icon={faArrowRight} className="w-3 mx-1 text-palette-primary"/>
-        </button>
+        </Carousel>
       </div>
+      {/*<div className="relative h-96">*/}
+      {/*  {*/}
+      {/*    mainImg && <Image*/}
+      {/*      src={mainImg}*/}
+      {/*      alt={mainImg}*/}
+      {/*      fill*/}
+      {/*      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"*/}
+      {/*      className="object-contain transform cursor-pointer"*/}
+      {/*    />}*/}
+      {/*</div>*/}
+      {/*<div className="relative flex border-t border-palette-lighter">*/}
+      {/*  <button*/}
+      {/*    aria-label="left-scroll"*/}
+      {/*    className="h-32 bg-gray-200 hover:bg-palette-light absolute left-0 z-10 opacity-75"*/}
+      {/*    onClick={() => scroll(-200)}*/}
+      {/*  >*/}
+      {/*    <FontAwesomeIcon icon={faArrowLeft} className="w-3 mx-1 text-palette-primary"/>*/}
+      {/*  </button>*/}
+      {/*  <div*/}
+      {/*    ref={ref}*/}
+      {/*    style={{scrollBehavior: "smooth"}}*/}
+      {/*    className="flex space-x-1 gap-5 w-full overflow-auto scrollbar-hide border-t border-palette-lighter"*/}
+      {/*  >*/}
+      {/*    {*/}
+      {/*      images && images.map((imgItem, index) => (*/}
+      {/*        <button*/}
+      {/*          key={index}*/}
+      {/*          className="relative w-40 h-32 flex-shrink-0 rounded-sm border border-palette-lighter"*/}
+      {/*          onClick={() => setMainImg(imgItem)}*/}
+      {/*        >*/}
+      {/*          <Image*/}
+      {/*            src={imgItem}*/}
+      {/*            alt={imgItem}*/}
+      {/*            fill*/}
+      {/*            className="object-cover"*/}
+      {/*            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"*/}
+      {/*          />*/}
+      {/*        </button>*/}
+      {/*      ))*/}
+      {/*    }*/}
+      {/*  </div>*/}
+      {/*  <button*/}
+      {/*    aria-label="right-scroll"*/}
+      {/*    className="h-32 bg-gray-200 hover:bg-palette-light absolute right-0 z-10 opacity-75"*/}
+      {/*    onClick={() => scroll(200)}*/}
+      {/*  >*/}
+      {/*    <FontAwesomeIcon icon={faArrowRight} className="w-3 mx-1 text-palette-primary"/>*/}
+      {/*  </button>*/}
+      {/*</div>*/}
     </div>
   )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -31,20 +31,9 @@ export default function Login() {
               <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
                 {
                   errors.length > 0 &&
-                  <div
-                    className="mb-6 flex items-center p-4 text-sm text-red-800 rounded-lg bg-red-50"
-                    role="alert">
-                    <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                    </svg>
-                    <span className="sr-only">Info</span>
-                    <div>
-                      {errors}
-                    </div>
-                  </div>
+                  <Alert color={'red'}>
+                    {errors}
+                  </Alert>
                 }
                 <Alert color="info">
                   <span className="font-medium">Account:</span>
